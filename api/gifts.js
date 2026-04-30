6cecece71e07a07d70164f6a8ae27b1f70c3c816
arteomsuharev-hash <arteom.suharev@gmail.com>
2026-04-30 23:18:59 +1000

    Update gifts.js

--- api/gifts.js
+++ api/gifts.js
@@ -1,18 +1,25 @@
-import { Api, TelegramClient } from 'telegram';
-import { StringSession } from 'telegram/sessions/index.js';
-
-const apiId = 35428941;
-const apiHash = 'ba211e1b4b260186488fe154a6ed7585';
-const SESSION = '1AgAOMTQ5LjE1NC4xNjcuNDEBu6AFlypebj02yFbir2nbQx9l7eKvXQNHiy+oo6sUKgMyb5xrf3JCVTapyianLZznaD4AbOdvN6z/KZ1SBZgS6J9uNUcwRVyOGxE88ion68H/6nML47mHeciTSyfCYHrSs86a7f0iqKQH4trOEInEPET7se31VJmeE0D0nKQJTW1q9SKRye0352h5D8M1ti2Iu+lvKk+YaWkQ1l+wAAmCqpLeK09nmtf4e0M6EifvphCEOuEcLxxepRz+XjgBuU61ACdSDuX4cr/zRyt6H9Lpt2nAsu3sCZxp9x1USPnb2U4kT05X4cUTAPiCnXz9n4fYB7FxR6JrqID3va1G1NAtQrM=';
+import { getClient } from "../lib/mtproto-client.js";
+import { Api } from "telegram";
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  
   try {
-    const client = new TelegramClient(new StringSession(SESSION), apiId, apiHash, { connectionRetries: 3 });
-    await client.connect();
+    const client = await getClient();
     const result = await client.invoke(new Api.payments.GetStarGifts({ hash: 0 }));
-    const gifts = result.gifts.map(g => ({ id: g.id.toString(), name: g.title, price: g.stars, icon: '🎁' }));
+    
+    const gifts = result.gifts.map(g => ({
+      id: g.id.toString(),
+      name: g.title,
+      price: g.stars,
+      is_unique: !!(g.attributes?.length),
+      sticker: g.sticker ? { id: g.sticker.id, accessHash: g.sticker.accessHash, dcId: g.sticker.dcId } : null,
+      background: g.background ? { center_color: g.background.center_color, edge_color: g.background.edge_color } : null,
+      attributes: g.attributes || null,
+    }));
+    
     res.json({ success: true, gifts });
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: error.message });
